e6a09707b6e31d445ffa138a07d777c38eb9c502
coolworld2015 <wintermute2015@ukr.net>
2016-09-19 14:13:52 +0300

    init

---
+++ index.ios.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import React, { Component } from 'react';
+
+import {
+  AppRegistry
+} from 'react-native';
+
+import App from './app/src/components/app/app';
+
+AppRegistry.registerComponent('rnHoroscope', () => App);
