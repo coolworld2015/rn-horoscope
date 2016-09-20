33ce61ee5d5cbff4d6072189920526cccc40d60b
coolworld2015 <wintermute2015@ukr.net>
2016-09-20 15:42:46 +0300

    [signs] - updated signs

--- app/src/components/signs/signs.js
+++ app/src/components/signs/signs.js
@@ -1,6 +1,6 @@
-'use strict'
+'use strict';
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     AppRegistry,
     StyleSheet,
@@ -21,7 +21,7 @@ import {
 import MoviesDetails from './moviesDetails';
 
 class Signs extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         var ds = new ListView.DataSource({
@@ -31,52 +31,91 @@ class Signs extends Component {
         this.state = {
             dataSource: ds.cloneWithRows([]),
             searchQuery: props.searchQuery,
-						resultsCount: 0
+            resultsCount: 0
         };
     }
 
     componentWillMount() {
-      this.getSignsList();
+        this.getSignsList();
     }
 
     getSignsList() {
-      var arrSigns = [
-        {
-          name: 'Aries',
-          interval: '03/21 - 04/19',
-          pic: './Aries.jpg'
-        },
-        {
-          name: 'Taurus',
-          interval: '04/20 - 05/20',
-          pic: './Aries.jpg'
-        }
-      ];
+        var arrSigns = [
+            {
+                name: 'Aries',
+                interval: '03/21 - 04/19'
+            },
+            {
+                name: 'Taurus',
+                interval: '04/20 - 05/20'
+            },
+            {
+                name: 'Gemini',
+                interval: '05/21 - 06/21'
+            },
+            {
+                name: 'Cancer',
+                interval: '06/22 - 07/22'
+            },
+            {
+                name: 'Leo',
+                interval: '07/23 - 08/22'
+            },
+            {
+                name: 'Virgo',
+                interval: '08/23 - 09/22'
+            },
+            {
+                name: 'Libra',
+                interval: '09/23 - 10/22'
+            },
+            {
+                name: 'Scorpio',
+                interval: '10/23 - 11/21'
+            },
+            {
+                name: 'Sagittarius',
+                interval: '11/22 - 12/21'
+            },
+            {
+                name: 'Capricorn',
+                interval: '12/22 - 01/19'
+            },
+            {
+                name: 'Aquarius',
+                interval: '01/20 - 02/18'
+            },
+            {
+                name: 'Pisces',
+                interval: '02/19 - 03/20'
+            }
+        ];
 
-      this.setState({
-        dataSource: this.state.dataSource.cloneWithRows(arrSigns),
-        resultsCount: arrSigns.length,
-        responseData: arrSigns
-      });
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(arrSigns),
+            resultsCount: arrSigns.length,
+            responseData: arrSigns
+        });
     }
 
-    pressRow(rowData){
+    pressRow(rowData) {
         this.props.navigator.push({
             title: rowData.trackName,
             component: MoviesDetails,
             rightButtonTitle: 'Delete',
             onRightButtonPress: () => {
-              Alert.alert(
-                'Delete',
-                'Are you sure you want to delete ' + rowData.trackName + '?',
-                [
-                  {text: 'Cancel', onPress: () => console.log('Cancel Pressed!')},
-                  {text: 'OK', onPress: () => {
-                    this.deleteMovie(rowData.trackId);
-                    }
-                  },
-                ]
-              );
+                Alert.alert(
+                    'Delete',
+                    'Are you sure you want to delete ' + rowData.trackName + '?',
+                    [
+                        {text: 'Cancel', onPress: () => console.log('Cancel Pressed!')},
+                        {
+                            text: 'OK', onPress: () => {
+                            this.deleteMovie(rowData.trackId);
+                        }
+                        },
+                    ]
+                );
             },
             passProps: {
                 pushEvent: rowData
@@ -84,171 +123,210 @@ class Signs extends Component {
         });
     }
 
-    renderRow(rowData){
+    renderRow(rowData) {
         var image = <View />;
 
         switch (rowData.name) {
-          case 'Aries':
-            image = <Image
-                    source={require('../../../img/Aries.jpg')}
-                    style={{
-                      height: 115,
-                      width: 105,
-                      borderRadius: 20,
-                      margin: 20
-                      }}
-                  />;
-            break;
+            case 'Aries':
+                image = <Image source={require('../../../img/Aries.jpg')}
+                               style={styles.img}/>;
+                break;
 
             case 'Taurus':
-              image = <Image
-                      source={require('../../../img/Taurus.jpg')}
-                      style={{
-                        height: 115,
-                        width: 105,
-                        borderRadius: 20,
-                        margin: 20
-                        }}
-                    />;
-              break;
-          }
+                image = <Image source={require('../../../img/Taurus.jpg')}
+                               style={styles.img}/>;
+                break;
+
+            case 'Gemini':
+                image = <Image source={require('../../../img/Gemini.jpg')}
+                               style={styles.img}/>;
+                break;
+
+            case 'Cancer':
+                image = <Image source={require('../../../img/Cancer.jpg')}
+                               style={styles.img}/>;
+                break;
+
+            case 'Leo':
+                image = <Image source={require('../../../img/Leo.jpg')}
+                               style={styles.img}/>;
+                break;
+
+            case 'Virgo':
+                image = <Image source={require('../../../img/Virgo.jpg')}
+                               style={styles.img}/>;
+                break;
+
+            case 'Libra':
+                image = <Image source={require('../../../img/Libra.jpg')}
+                               style={styles.img}/>;
+                break;
+
+            case 'Scorpio':
+                image = <Image source={require('../../../img/Scorpio.jpg')}
+                               style={styles.img}/>;
+                break;
+
+            case 'Sagittarius':
+                image = <Image source={require('../../../img/Sagittarius.jpg')}
+                               style={styles.img}/>;
+                break;
+
+            case 'Capricorn':
+                image = <Image source={require('../../../img/Capricorn.jpg')}
+                               style={styles.img}/>;
+                break;
+
+            case 'Aquarius':
+                image = <Image source={require('../../../img/Aquarius.jpg')}
+                               style={styles.img}/>;
+                break;
+
+            case 'Pisces':
+                image = <Image source={require('../../../img/Pisces.jpg')}
+                               style={styles.img}/>;
+                break;
+
+        }
 
         return (
-          	<TouchableHighlight
+            <TouchableHighlight
                 onPress={()=> this.pressRow(rowData)}
                 underlayColor='#ddd'
-          	>
+            >
 
-            <View style={styles.imgsList}>
+                <View style={styles.imgsList}>
 
-              {image}
+                    {image}
 
-              <View style={{
-                 flex: 1,
-                 flexDirection: 'column',
-                 justifyContent: 'space-between'
-                }}>
-                  <Text>{rowData.name}</Text>
-                  <Text>{rowData.interval}</Text>
-              </View>
+                    <View style={{
+                        flex: 1,
+                        flexDirection: 'column',
+                        justifyContent: 'space-between'
+                    }}>
+                        <Text>{rowData.name}</Text>
+                        <Text>{rowData.interval}</Text>
+                    </View>
 
-            </View>
-          </TouchableHighlight>
+                </View>
+            </TouchableHighlight>
         );
     }
 
-    refreshData(event){
-      if (event.nativeEvent.contentOffset.y <= -100) {
-        setTimeout(() => {this.getSignsList()}, 300);
-      }
+    refreshData(event) {
+        if (event.nativeEvent.contentOffset.y <= -100) {
+            setTimeout(() => {
+                this.getSignsList()
+            }, 300);
+        }
     }
 
-    render(){
-      var errorCtrl = <View />;
+    render() {
+        var errorCtrl = <View />;
 
-        if(this.state.serverError){
+        if (this.state.serverError) {
             errorCtrl = <Text style={styles.error}>
                 Something went wrong.
             </Text>;
         }
 
-      if(this.state.showProgress){
+        if (this.state.showProgress) {
+            return (
+                <View style={{
+                    flex: 1,
+                    justifyContent: 'center'
+                }}>
+                    <ActivityIndicator
+                        size="large"
+                        animating={true}/>
+                </View>
+            );
+        }
+
         return (
-            <View style={{
-                flex: 1,
-                justifyContent: 'center'
-            }}>
-                <ActivityIndicator
-                    size="large"
-                    animating={true} />
+            <View style={{flex: 1, justifyContent: 'center'}}>
+                <View style={{marginTop: 60}}>
+                    <TextInput style={{
+                        height: 45,
+                        marginTop: 4,
+                        padding: 5,
+                        backgroundColor: 'white',
+                        borderWidth: 3,
+                        borderColor: 'lightgray',
+                        borderRadius: 0
+                    }}
+                               onChangeText={(text)=> {
+                                   if (this.state.responseData == undefined) {
+                                       return;
+                                   }
+                                   var arr = [].concat(this.state.responseData);
+                                   var items = arr.filter((el) => el.name.indexOf(text) >= 0);
+                                   this.setState({
+                                       dataSource: this.state.dataSource.cloneWithRows(items),
+                                       resultsCount: items.length,
+                                   })
+                               }}
+                               placeholder="Search here">
+                    </TextInput>
+
+                    {errorCtrl}
+
+                </View>
+
+                <ScrollView
+                    onScroll={this.refreshData.bind(this)} scrollEventThrottle={16}
+                    style={{marginTop: 0, marginBottom: 0}}>
+                    <ListView
+                        style={{marginTop: -65, marginBottom: 0}}
+                        dataSource={this.state.dataSource}
+                        renderRow={this.renderRow.bind(this)}
+                    />
+                </ScrollView>
+
+                <View style={{marginBottom: 49}}>
+                    <Text style={styles.countFooter}>
+                        {this.state.resultsCount} entries were found.
+                    </Text>
+                </View>
+
             </View>
-        );
-      }
-        return (
-          <View style={{flex: 1, justifyContent: 'center'}}>
-            <View style={{marginTop: 60}}>
-				 <TextInput style={{
-					  height: 45,
-					  marginTop: 4,
-					  padding: 5,
-					  backgroundColor: 'white',
-					  borderWidth: 3,
-					  borderColor: 'lightgray',
-					  borderRadius: 0,
-					}}
-				  onChangeText={(text)=> {
-            if (this.state.responseData == undefined) {
-              return;
-            }
-					  var arr = [].concat(this.state.responseData);
-					  var items = arr.filter((el) => el.name.indexOf(text) >= 0);
-					  this.setState({
-						 dataSource: this.state.dataSource.cloneWithRows(items),
-						 resultsCount: items.length,
-					  })
-          }}
-          placeholder="Search here">
-              </TextInput>
-
-          	{errorCtrl}
-
-            </View>
-
-          <ScrollView
-            onScroll={this.refreshData.bind(this)} scrollEventThrottle={16}
-            style={{marginTop: 0, marginBottom: 0}}>
-            <ListView
-              style={{marginTop: -65, marginBottom: 0}}
-              dataSource={this.state.dataSource}
-              renderRow={this.renderRow.bind(this)}
-            />
-  				</ScrollView>
-
-          <View style={{marginBottom: 49}}>
-            <Text style={styles.countFooter}>
-              {this.state.resultsCount} entries were found.
-            </Text>
-          </View>
-
-        </View>
-      )
-	}
+        )
+    }
 }
-
 
 const styles = StyleSheet.create({
     imgsList: {
-      flex: 1,
-      flexDirection: 'row',
-      padding: 0,
-      alignItems: 'center',
-      borderColor: '#D7D7D7',
-      borderBottomWidth: 1,
-      backgroundColor: '#fff'
+        flex: 1,
+        flexDirection: 'row',
+        padding: 0,
+        alignItems: 'center',
+        borderColor: '#D7D7D7',
+        borderBottomWidth: 1,
+        backgroundColor: '#fff'
     },
     countHeader: {
-      fontSize: 16,
-      textAlign: 'center',
-      padding: 15,
-      backgroundColor: '#F5FCFF',
+        fontSize: 16,
+        textAlign: 'center',
+        padding: 15,
+        backgroundColor: '#F5FCFF',
     },
-  	countFooter: {
-      fontSize: 16,
-      textAlign: 'center',
-      padding: 10,
-      borderColor: '#D7D7D7',
-      backgroundColor: 'whitesmoke'
+    countFooter: {
+        fontSize: 16,
+        textAlign: 'center',
+        padding: 10,
+        borderColor: '#D7D7D7',
+        backgroundColor: 'whitesmoke'
     },
     img: {
-      height: 95,
-      width: 75,
-      borderRadius: 20,
-      margin: 20
+        height: 115,
+        width: 105,
+        borderRadius: 20,
+        margin: 20
     },
     error: {
-      color: 'red',
-      paddingTop: 10,
-      textAlign: 'center'
+        color: 'red',
+        paddingTop: 10,
+        textAlign: 'center'
     }
 });
 
